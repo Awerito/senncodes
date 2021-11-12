a80f55588124c44528902bf2df14948d8ca96021
Awerito <diego.i.munoz.viveros@gmail.com>
2021-11-12 16:29:41 -0300

    Fix: sample notation

--- sample_secrets.py
+++ sample_secrets.py
@@ -1,17 +1,17 @@
-subscribe_url = # subscribe url
-validate_code_url = # validate url
+subscribe_url = # "subscribe url"
+validate_code_url = # "validate url"
 
 submit_info = {
-    "first_name": # first_name
-    "last_name": # last_name
-    "email": # email
+    "first_name": # "first_name",
+    "last_name": # "last_name",
+    "email": # "email",
 }
 
 headers = {
     "authority": "www.sennheisertreasurehunt.com",
     "accept": "application/json, text/plain, */*",
-    "x-xsrf-token": # xsrf-token
-    "user-agent": # user-agen
+    "x-xsrf-token": # "xsrf-token",
+    "user-agent": # "user-agen",
     "content-type": "application/json;charset=UTF-8",
     "sec-gpc": "1",
     "origin": "https://www.sennheisertreasurehunt.com",
@@ -20,5 +20,5 @@ headers = {
     "sec-fetch-dest": "empty",
     "referer": "https://www.sennheisertreasurehunt.com/en",
     "accept-language": "en-US,en;q=0.9,es;q=0.8",
-    "cookie": # your cookie
+    "cookie": # "your cookies",
 }
